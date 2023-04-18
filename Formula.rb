0ef0c78da4d1451d0d103839307ece989ab0ed74
Cyber OS Lab <99838653+cyberoslab@users.noreply.github.com>
2023-04-17 17:25:06 -0700

    Update Formula.rb
    Signed-off-by: Cyber OS Lab. <99838653+cyberoslab@users.noreply.github.com>

--- Formula.rb
+++ Formula.rb
@@ -1,7 +1,7 @@
 class scan4all < Formula
     desc "Official repository vuls Scan: 15000+PoCs; 23 kinds of application password crack; 7000+Web fingerprints; 146 protocols and 90000+ rules Port scanning; Fuzz, HW, awesome BugBounty( ͡° ͜ʖ ͡°)..."
     homepage "https://github.com/w3security/goscan"
-    url "https://github.com/w3security/scan4all/releases/download/2.8.5/goscan_2.1.1_macOS_amd64.zip"
+    url "https://github.com/w3security/goscan/releases/download/2.8.5/goscan_2.1.1_macOS_amd64.zip"
     sha256 "ccd874a283defad6a0deb11377cb9d6024cb5946b46f61f36008e0afe9db4950"
     version "V2.1.1"
 
